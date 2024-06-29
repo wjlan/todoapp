806cf5331414597dd4d54c5e57a205c44695f04c
wanjun <wanjunlan@gmail.com>
2024-06-28 22:31:50 -0700

    add TodoController to handle HTTP requests for Todo entities

---
+++ src/main/java/dev/wanjun/todoapp/controller/TodoController.java
@@ -0,0 +1,35 @@
+package dev.wanjun.todoapp.controller;
+
+import dev.wanjun.todoapp.domain.Todo;
+import dev.wanjun.todoapp.service.TodoService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+    @Resource
+    private TodoService todoService;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo updatedTodo) {
+        return todoService.updateTodo(id, updatedTodo);
+    }
+
+    @DeleteMapping
+    public void deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+    }
+}
